bbefbcab31f2cf5cc6beea1b35310e11bef30151
Matjaž Mav <matjaz.mav@gmail.com>
2019-03-30 17:45:53 +0100

    Added comment and function to draw square in the size of the kernel

--- implementations/mean_shift_mode.m
+++ implementations/mean_shift_mode.m
@@ -1,11 +1,14 @@
+% Click on figure to trigger mean shift
+
 load('helpers/tabulated.mat');
 
 kernel = ones(20);%create_epanechnik_kernel(10, 10, 0.5);
 
 figure(1);
 hold on;
+imagesc(responses, 'ButtonDownFcn', {@button_down_handler,responses,kernel});
+axis([0 100 0 100]);
 axis square;
-h = imagesc(responses, 'ButtonDownFcn', {@button_down_handler,responses,kernel});
 
 function button_down_handler(src, eventdata, relief, kernel)
     coordinates = get(get(src,'Parent'),'CurrentPoint'); 
@@ -13,10 +16,14 @@ function button_down_handler(src, eventdata, relief, kernel)
     x = coordinates(1);
     y = coordinates(2);
 
+    % Plot click point & square of neighbourhood
     plot(x, y, 'rx','MarkerSize', 20);
+    plot_square(x, y, size(kernel, 1))
     
+    % Find path
     [xs, ys] = find_mode([x y], relief, kernel);
     
+    % Plot path
     plot(xs, ys, '-r.','MarkerSize',8);
 end
 
@@ -31,26 +38,41 @@ function [xs, ys] = find_mode(start, relief, kernel)
            
     while true
         
-        frame_x = round(xs(end)-n/2);
-        frame_y = round(ys(end)-n/2);
-        
-        frame = relief(...
-            [frame_y:frame_y+n-1],...
-            [frame_x:frame_x+n-1]...
-            );
+        % Extract neighbourhood
+        [fx1, fy1, fx2, fy2] = square_points(xs(end), ys(end), n)  ;      
+        frame = relief([fy1:fy2], [fx1:fx2]);
 
-        x_n = round(sum((mx+frame_x).*frame)/sum(frame));
-        y_n = round(sum((my+frame_y).*frame)/sum(frame));
+        % Calculate new position
+        x_n = round(sum((mx+fx1).*frame)/sum(frame));
+        y_n = round(sum((my+fy1).*frame)/sum(frame));
 
+        % Calculate displacement vector
         dx = x_n - xs(end);
         dy = y_n - ys(end);
-
+        
+        % Stop when displacement vector converges
         if dx == 0 && dy == 0
             break
         end  
                 
+        % Append new step
         xs = [xs x_n];
         ys = [ys y_n];
     end
+end
+
+function [x1, y1, x2, y2] = square_points(x, y, n)
+    x1 = round(x-n/2);
+    y1 = round(y-n/2);
+    x2 = x1 + n-1;
+    y2 = y1 + n-1;
+end
+
+function plot_square(x, y, n)
+    [x1, y1, x2, y2] = square_points(x, y, n);
+    
+    x = [x1, x2, x2, x1, x1];
+    y = [y1, y1, y2, y2, y1];
+    plot(x, y, '--r', 'LineWidth', 1);
 end
 
